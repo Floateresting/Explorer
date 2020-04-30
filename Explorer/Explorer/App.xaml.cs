febce47c8ead11ac32441474558bbf2659ddf6e1
unknown <eiqytruwop@outlook.com>
2020-04-30 12:05:37 -0300

    Initial template

---
+++ Explorer/Explorer/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Explorer.Services;
+using Explorer.Views;
+
+namespace Explorer {
+    public partial class App : Application {
+
+        public App() {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart() {
+        }
+
+        protected override void OnSleep() {
+        }
+
+        protected override void OnResume() {
+        }
+    }
+}
---
+++ Explorer/Explorer/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using Explorer.Models;
+
+namespace Explorer.ViewModels {
+    public class ItemDetailViewModel : BaseViewModel {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null) {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
